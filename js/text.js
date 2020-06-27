6f21954b4594328638dabe697d3765044fc8de38
GabrielAdami <jose_adami@live.com>
2020-06-27 15:29:21 -0300

    subindo site

---
+++ js/text.js
@@ -0,0 +1,11 @@
+    function typeWriter(elemento){
+        const textotoArray = elemento.innerHTML.split(" ");
+        elemento.innerHTML = '';
+        textotoArray.forEach((letra, i) => {
+            setTimeout(() => elemento.innerHTML += letra, 75 * i);
+        });
+
+    }
+const titulo = document.getElementById("titulo");
+console.log(titulo);
+
